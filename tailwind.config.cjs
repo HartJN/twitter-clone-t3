2d7b4a02b05ea221737cc408bab22adc69ef47a0
HartJN <jameshart04@gmail.com>
2023-02-26 17:14:30 +1300

    chore: adds custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e88e5',
+        secondary: '#ff5722',
+        success: '#4caf50',
+        info: '#2196f3',
+        warning: '#ff9800',
+        danger: '#f44336',
+      },
+    },
   },
   plugins: [],
 };
