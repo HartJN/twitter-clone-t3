401716e2bd803d497d7bbf9afe45e1f89dedaa4d
HartJN <jameshart04@gmail.com>
2023-02-26 17:22:09 +1300

    feat: adds container as provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
+import { Container } from '../components/Container';
 
 import { api } from '~/utils/api';
 
 import '~/styles/globals.css';
+import { LoggedOutBanned } from '~/components/LoggedOutBanner';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +15,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Container>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </Container>
+      <LoggedOutBanned />
+      <ReactQueryDevtools initialIsOpen={false} />
     </SessionProvider>
   );
 };
