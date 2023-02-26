b29a9c9b081ad3c1f346a4757da75b63dc4c5e62
HartJN <jameshart04@gmail.com>
2023-02-26 17:48:30 +1300

    feat: adds selective loading

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -24,18 +24,28 @@ export const tweetRouter = createTRPCRouter({
   timeline: publicProcedure
     .input(
       z.object({
+        where: z
+          .object({
+            author: z
+              .object({
+                name: z.string().optional(),
+              })
+              .optional(),
+          })
+          .optional(),
         cursor: z.string().nullish(),
         limit: z.number().min(1).max(100).default(10),
       })
     )
     .query(async ({ ctx, input }) => {
       const { prisma } = ctx;
-      const { cursor, limit } = input;
+      const { cursor, limit, where } = input;
 
       const userId = ctx.session?.user?.id;
 
       const tweets = await prisma.tweet.findMany({
         take: limit + 1,
+        where,
         orderBy: [
           {
             createdAt: 'desc',
